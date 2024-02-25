705f420b6aeaea85945d5d5b774c8fddbacb8fff
Akhil Vudatha <akhilvsvs1999@gmail.com>
2024-02-26 01:40:55 +0530

    Initial Commit

---
+++ src/main/java/com/scrum/tasks/ScrumTasksApplication.java
@@ -0,0 +1,13 @@
+package com.scrum.tasks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrumTasksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScrumTasksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scrum/tasks/ScrumTasksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scrum.tasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScrumTasksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
